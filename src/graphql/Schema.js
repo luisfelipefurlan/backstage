31de356966c39a224647c37d1e07557ee9e3eea6
Julia <jmourac@cpqd.com.br>
2020-05-15 11:24:17 -0300

    feat: add get device by id, get devices and get history - GraphQL (#33)
    * Added the types used by devices, the getDEvices query and resolvers
    
    * Adding getDeviceById query and resolver
    
    * getDevice and getDeviceById with tests
    
    * Adding getDeviceHistory to Resolvers
    
    * getDeviceHistory returning array of History
    
    * Added user configuration files and reference to Schema
    
    * added suggested configuration in package.json and device resolver
    
    * added suggested configuration in package.json and device resolver
    
    * Update package.json
    
    * removed history queries
    
    * removed history queries
    
    * removed history queries
    
    Update package.json
    removed history queries
    
    * adjusted queries to right typedef version
    
    * Reimplemented getDeviceHistory
    
    * Unit tests for device resolvers
    
    * small fixes
    
    * Added new unit tests, comments on types and removed lines from Schema
    
    * Updated Schema, Resolvers and TypeDefs, and added Unit tests
    
    * Added/adjusted unit tests
    
    * somefix
    
    * fixed tests and device resolvers
    
    * fixes
    
    fixes
    
    Co-authored-by: Mariane Previde <mariane.previde@gmail.com>
    Co-authored-by: Mariane Previde <mprevide@cpqd.com.br>

--- src/graphql/Schema.js
+++ src/graphql/Schema.js
@@ -3,21 +3,30 @@ const { merge } = require('lodash');
 const commonTypeDefs = require('./common/TypeDefs');
 const templateTypeDefs = require('./template/TypeDefs');
 const templateResolvers = require('./template/Resolvers');
+const deviceTypeDefs = require('./device/TypeDefs');
+const deviceResolvers = require('./device/Resolvers');
 
 
 const query = [`
-type Query {
-   #Get a template by Id
-    template(id: Int!): Template
-    #Checks if templates has Image Firmware and return a array with objects key-value, where key is a id template and value is a boolean.
-    #The value is true if the template has image firmware.
-    templatesHasImageFirmware(templatesId: [Int]!): [MapStringToString]
-  }
+  type Query {
+      #Get a template by Id
+      template(id: Int!): Template
+      #Checks if templates has Image Firmware and return a array with objects key-value, where key is a id template and value is a boolean.
+      #The value is true if the template has image firmware.
+      templatesHasImageFirmware(templatesId: [Int]!): [MapStringToString]
+      #Returns a list of devices that can be divided in pages, and the information about how many pages there are in total, along with which page is being shown
+      getDevices(page: PageInput, filter: FilterDeviceInput): DeviceListPage
+      #Finds device information by id
+      getDeviceById(deviceId: String!): Device
+      #Returns historical data about devices' attributes chosen in the input
+      getDeviceHistory(filter: HistoryInput!): [History]
+    }
 `];
+
 // Put schema together into one array of schema strings
 // and one map of resolvers, like makeExecutableSchema expects
-const typeDefs = [...query, ...templateTypeDefs, ...commonTypeDefs];
-const resolvers = merge(templateResolvers);
+const typeDefs = [...query, ...templateTypeDefs, ...commonTypeDefs, ...deviceTypeDefs];
+const resolvers = merge(templateResolvers, deviceResolvers);
 
 const executableSchema = makeExecutableSchema({ typeDefs, resolvers });
 

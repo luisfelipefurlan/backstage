69168860c18aef6ab46a9f415604f14eb2f8d1cb
Mariane Previde <mprevide@cpqd.com.br>
2020-05-15 11:29:50 -0300

    Update deploy-gh-pages.sh

--- src/travis/deploy-gh-pages.sh
+++ src/travis/deploy-gh-pages.sh
@@ -10,15 +10,16 @@ if [ $TRAVIS_PULL_REQUEST == false ] ; then
 
   git remote add -t gh-pages -f origin-gh-pages https://github.com/${TRAVIS_REPO_SLUG}
   git fetch origin-gh-pages
-  #git checkout -- .
-  git checkout gh-pages
+  git checkout .
+  git checkout origin-gh-pages/gh-pages
+  
   git checkout ${TRAVIS_BRANCH} -- ./src
 
   node ./src/docs/GenerateFileSchema.js
   graphdoc -s ./src/docs/schemaDoc.graphql -o ./${VERSION} -f
   
   rm -fr ./_static
-  git rm -r src -f
+  git rm -r ./src -f
 
   git add ./${VERSION}
   git commit -m 'Updating gh-pages' --amend
